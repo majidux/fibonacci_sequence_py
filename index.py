6b56d321e02309055bb86247cbb4d484af1a5bc4
Majid <majiddarvishnejad93@gmail.com>
2021-07-04 10:12:31 +0430

    feat: delete useless code

--- index.py
+++ index.py
@@ -1,13 +1,3 @@
-
-# def Fibonacci2(n, memo = {}):
-#     if n <= 2:
-#         return 1
-#     else:
-#         return Fibonacci2(n-1)+Fibonacci2(n-2)
- 
-# print(Fibonacci2(123))
-
-
 def Fibonacci(num, memo = {}):
     if num in memo: return memo[num]
     elif num <= 2:
@@ -15,5 +5,3 @@ def Fibonacci(num, memo = {}):
     else:
         memo[num] = Fibonacci(num - 1, memo) + Fibonacci(num - 2, memo)
     return memo[num]
- 
-print(Fibonacci(1))
